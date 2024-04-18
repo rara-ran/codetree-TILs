0c3d8b88ff7c75c902249647f4645e683eadf359
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-18 14:22:04 +0900

    Title: 세 수의 최대 곱, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240418/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240418/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,13 +1,31 @@
 n = int(input())
 number = list(map(int, input().split()))
-plus_number = [x for x in number if x > 0]
-minus_number = [-x for x in number if x < 0]
 
-plus_number.sort()
-minus_number.sort()
-m = plus_number.pop()
+def make_maxV(n, number):
+    if n == 3:
+        return number.pop()*number.pop()*number.pop()
 
-plus_value = m*(plus_number.pop())*(plus_number.pop())
-minus_value = m*minus_number.pop()*minus_number.pop()
+    plus_number = [x for x in number if x > 0]
+    minus_number = [-x for x in number if x < 0]
 
-print(max(plus_value, minus_value))
+    plus_number.sort()
+    minus_number.sort()
+
+    if len(plus_number) >= 3 and len(minus_number) >= 2:
+        m = plus_number.pop()
+
+        plus_value = m*plus_number.pop()*plus_number.pop()
+        minus_value = m*minus_number.pop()*minus_number.pop()
+        return (max(plus_value, minus_value))
+
+    elif len(plus_number) >= 3 and len(minus_number) < 2:
+        return plus_number.pop()*plus_number.pop()*plus_number.pop()
+    
+    elif len(plus_number) <= 1:
+        if len(plus_number) == 1:
+            return plus_number.pop()*minus_number.pop()*minus_number.pop()
+        else:
+            return -minus_number.pop(0)*minus_number.pop(0)*minus_number.pop(0)
+
+
+print(make_maxV(n, number))

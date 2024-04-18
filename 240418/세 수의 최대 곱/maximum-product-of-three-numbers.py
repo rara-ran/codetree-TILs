0563ce69ec9257c0282edde564673ac8d372fe81
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-18 14:28:14 +0900

    Title: 세 수의 최대 곱, Time: 148ms, Memory: 31MB, Status: Passed - CodeTree

--- 240418/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240418/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,31 +1,11 @@
 n = int(input())
 number = list(map(int, input().split()))
 
-def make_maxV(n, number):
-    if n == 3:
-        return number.pop()*number.pop()*number.pop()
+number.sort()
 
-    plus_number = [x for x in number if x > 0]
-    minus_number = [-x for x in number if x < 0]
+a,b,c = number[-1], number[-2], number[-3]
+cost_1 = a*b*c
+x,y,z = number[0], number[1], number[-1]
+cost_2 = x*y*z
 
-    plus_number.sort()
-    minus_number.sort()
-
-    if len(plus_number) >= 3 and len(minus_number) >= 2:
-        m = plus_number.pop()
-
-        plus_value = m*plus_number.pop()*plus_number.pop()
-        minus_value = m*minus_number.pop()*minus_number.pop()
-        return (max(plus_value, minus_value))
-
-    elif len(plus_number) >= 3 and len(minus_number) < 2:
-        return plus_number.pop()*plus_number.pop()*plus_number.pop()
-    
-    elif len(plus_number) <= 1:
-        if len(plus_number) == 1:
-            return plus_number.pop()*minus_number.pop()*minus_number.pop()
-        elif len(plus_number) == 0:
-            return -minus_number.pop(0)*minus_number.pop(0)*minus_number.pop(0)
-
-
-print(make_maxV(n, number))
+print(max(cost_1, cost_2))

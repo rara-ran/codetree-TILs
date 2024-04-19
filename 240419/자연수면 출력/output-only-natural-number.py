1b310a93ed05b41ed0f8200f5ad7ca0caef67f9a
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-19 17:18:18 +0900

    Title: 자연수면 출력, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/자연수면 출력/output-only-natural-number.py
+++ 240419/자연수면 출력/output-only-natural-number.py
@@ -1,6 +1,10 @@
 a, b= map(int, input().split())
 answer = ''
-for i in range(b):
-    answer += str(a)
+
+if a == 0:
+    answer = 0
+else:
+    for i in range(b):
+        answer += str(a)
 
 print(answer)

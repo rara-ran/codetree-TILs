cc03cd687df0a19abae548f9f36bbb623297d7d1
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-19 09:56:25 +0900

    Title: 최소 와이파이 수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240419/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,27 +1,35 @@
 def install_wifi(n, m, number):
-    wifi = 0
-    people_idx = 0
-    if m == 0:
+    
+    if m == 0: # 0위치 이내
         return number.count(1)
-    idx = 0
-    while number.count(0) != n:
-        if number[idx] == 1:
-            people_idx += 1
-        idx += 1
 
-        if people_idx == m:
-            if idx+m < n and idx-m >=0:
-                for i in range(idx-m, idx+m+1): # 
-                    number[i] = 0
-            else: 
-                if idx+m >= n:
-                    for i in range(idx-m, n):
+    if number.count(0) == n: # 아무도 안 살면
+        return 0
+
+    wifi = 0
+    people_idx = []
+    idx = 0
+
+    while idx < n:
+        if number[idx] == 1:
+            people_idx.append(idx)
+        idx += 1
+        if people_idx:
+            if idx - people_idx[0] == m :
+                if idx+m < n and idx-m >=0:
+                    for i in range(idx-m, idx+m+1): # 
                         number[i] = 0
-                elif idx-m < 0:
-                    for i in range(idx+m+1):
-                        number[i] = 0
-            people_idx = 0
-            wifi += 1
+                else: 
+                    if idx+m >= n:
+                        for i in range(idx-m, n):
+                            number[i] = 0
+                    elif idx-m < 0:
+                        for i in range(idx+m+1):
+                            number[i] = 0
+                people_idx = []
+                wifi += 1
+
+        # print(number)
     return wifi
 
 n, m = map(int, input().split())

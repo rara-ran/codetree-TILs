c2a6d75027a7e851a3f7fa082a17233de43943a8
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-24 13:33:01 +0900

    Title: 별 찍기 1, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/별 찍기 1/star-make-1.py
@@ -0,0 +1,13 @@
+n, t = map(int, input().split())
+
+if t == 1:
+    for i in range(1, n+1):
+        print('*'*i)
+
+elif t == 2:
+    for i in range(n, 0, -1):
+        print('*'*i)
+
+elif t == 3:
+    for i in range(1, n+1):
+        print(' '*(n-i), '*'*(2*i-1))

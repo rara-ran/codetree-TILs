64525f5801f5cca79d70b2a3db89c3b69afd35e0
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-25 00:18:19 +0900

    Title: 색맹, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240425/색맹/color-blindness.py
@@ -0,0 +1,69 @@
+from collections import deque
+
+def make_rgb(color,tx,ty): # R, G, B 세가지
+
+    dx, dy = [-1,1,0,0], [0,0,-1,1]
+
+    q = deque()
+    q.append((tx,ty))
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = dx[i]+x, dy[i]+y
+
+            if nx < 0 or ny < 0 or nx >= n or ny>=n:
+                continue
+
+            if maps[nx][ny] == color and rgb_visited[nx][ny] == 0:
+                rgb_visited[nx][ny] = 1
+                q.append((nx, ny))
+        
+
+def make_rg(tx,ty): # R, G 하나로
+    dx, dy = [-1,1,0,0], [0,0,-1,1]
+
+    q = deque()
+    q.append((tx,ty))
+    
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = dx[i]+x, dy[i]+y
+
+            if nx < 0 or ny < 0 or nx >= n or ny>=n:
+                continue
+
+            if maps[nx][ny] in ('R', 'G') and rgb_visited[nx][ny] == 1:
+                rgb_visited[nx][ny] = 2
+                q.append((nx, ny))
+        
+
+n = int(input())
+maps = [list(input()) for _ in range(n)]
+rgb_visited = [[0]*n for _ in range(n)]
+rg_visited = [[0]*n for _ in range(n)]
+rgb_count, rg_count = 0, 0
+
+for i in range(n):
+    for j in range(n):
+        if rgb_visited[i][j] == 0 and maps[i][j] == 'B':
+            make_rgb('B', i, j)
+            rgb_count+=1
+            rg_count+=1
+        elif rgb_visited[i][j] == 0 and maps[i][j] == 'R':
+            make_rgb('R', i, j)
+            rgb_count += 1
+        elif rgb_visited[i][j] == 0 and maps[i][j] == 'G':
+            make_rgb('G', i, j)
+            rgb_count += 1
+
+
+for i in range(n):
+    for j in range(n):
+        if rgb_visited[i][j] == 1 and maps[i][j] in ('R', 'G'):
+            make_rg(i,j)
+            rg_count+=1
+        
+
+print(rgb_count, rg_count)

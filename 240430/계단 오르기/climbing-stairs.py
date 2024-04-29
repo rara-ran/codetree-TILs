768adb61fc625ac75121225afce575c114d2308e
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-30 00:17:08 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240430/계단 오르기/climbing-stairs.py
+++ 240430/계단 오르기/climbing-stairs.py
@@ -1,12 +1,17 @@
 n = int(input())
+memo = [-1]*(n+1)
 
 def stair(n):
+    if memo[n] != -1:
+        return memo[n]
     if n == 2 or n == 3:
         return 1
     if n == 1:
         return 0
     else:
-        return stair(n-2) + stair(n-3)
+        memo[n] = stair[n-2] + stair[n-3]
+    
+    return memo[n]
 
     
 print(stair(n)%10007)

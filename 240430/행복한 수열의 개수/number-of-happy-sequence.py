c801e0d3386f5d2bcf10949038f70410ca10d21f
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-30 13:48:37 +0900

    Title: 행복한 수열의 개수, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240430/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,16 +3,17 @@ def count_num(arr, n, k):
     cnt_arr = []
     cnt = 1
     for i in range(n-1):
+
         if arr[i] == arr[i+1]:
-            cnt+=1
+            cnt += 1
         else:
             cnt_arr.append(cnt)
             cnt = 1
-    if cnt:
-        cnt_arr.append(cnt)
-    
-    ans_arr = [1 for x in cnt_arr if x >= k]
 
+    cnt_arr.append(cnt)
+
+    ans_arr = [x for x in cnt_arr if x >= k]
+    
     if ans_arr:
         return 1
     else:
@@ -23,14 +24,16 @@ def count_num(arr, n, k):
 n, k = map(int, input().split())
 maps = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
-row_check = [[0] for _ in range(n)]
+row_check = [[] for _ in range(n)]
 
 for col in range(n):
     for idx in range(n):
         row_check[idx].append(maps[col][idx])
     answer += (count_num(maps[col], n, k))
 
+
 for row in range(n):
     answer += (count_num(row_check[row],n,k))
+
 
 print(answer)

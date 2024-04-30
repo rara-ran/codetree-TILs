6d5d6eba60a7c1439d39a436342d84b43f067b5a
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-04-30 09:14:43 +0900

    Title: 계단 오르기, Time: 5036ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240430/계단 오르기/climbing-stairs.py
+++ 240430/계단 오르기/climbing-stairs.py
@@ -2,16 +2,13 @@ n = int(input())
 memo = [-1]*(n+1)
 
 def stair(n):
-    if memo[n] != -1:
-        return memo[n]
-    if n == 2 or n == 3:
-        return 1
-    elif n == 1:
+    if n == 1:
         return 0
+    elif n==2 or n==3:
+        return 1
     else:
-        memo[n] = stair(n-2) + stair(n-3)
+        memo[n] = stair(n-3) + stair(n-2)
     
     return memo[n]
 
-    
 print(stair(n)%10007)

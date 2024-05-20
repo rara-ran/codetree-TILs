26afa2bc0752810b322677b28cc5db60cff1d03b
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-05-20 17:14:08 +0900

    Title: 정수 사각형 최소 합, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240520/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -0,0 +1,17 @@
+n = int(input())
+maps = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0]*n for _ in range(n)]
+
+dp[0][n-1] = maps[0][n-1]
+
+for j in range(n-2, -1, -1):
+    dp[0][j] = dp[0][j+1] + maps[0][j]
+
+for i in range(1, n):
+    dp[i][n-1] = dp[i-1][n-1] + maps[i][n-1]
+
+for i in range(1, n):
+    for j in range(n-2, -1, -1):
+        dp[i][j] = min(dp[i-1][j], dp[i][j+1]) + maps[i][j]
+print(dp[n-1][0])

6d1aaa0ef54182fe6f60ccec33c71d85a1446b2f
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-05-22 10:49:24 +0900

    Title: 알파벳 출력하기 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/알파벳 출력하기 2/print-alphabet-2.py
+++ 240522/알파벳 출력하기 2/print-alphabet-2.py
@@ -1,10 +1,12 @@
 n = int(input())
 
 arr = [['']*n for _ in range(n)]
-num = 65
+num = 64
 for i in range(n):
     print('  '*i, end='')
     for j in range(n-i):
-        print(chr(num+j), end=' ')
-    num += (j+1)
+        num += 1
+        if num > 90:
+            num = 65
+        print(chr(num), end=' ')
     print()

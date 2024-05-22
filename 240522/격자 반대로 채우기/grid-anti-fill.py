7f4eb15fcff671ae8790162c67e93499446df587
HyeRan <60253316+rara-ran@users.noreply.github.com>
2024-05-22 13:40:29 +0900

    Title: 격자 반대로 채우기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/격자 반대로 채우기/grid-anti-fill.py
+++ 240522/격자 반대로 채우기/grid-anti-fill.py
@@ -1,18 +1,39 @@
+# 홀수일 때
+def odd_num(n, maps):
+    num = n**2
+    for j in range(n):
+        if j % 2 == 0:
+            for i in range(n):
+                maps[i][j] = num
+                num -= 1
+        else:
+            for x in range(n-1, -1, -1):
+                maps[x][j] = num
+                num -= 1
+    return maps
+
+# 짝수일 때
+def even_num(n, maps):
+    num = n**2
+    for j in range(n):
+        if j % 2 == 1:
+            for i in range(n):
+                maps[i][j] = num
+                num -= 1
+        else:
+            for x in range(n-1, -1, -1):
+                maps[x][j] = num
+                num -= 1
+    return maps
+
 n = int(input())
 
 maps = [[0]*n for _ in range(n)]
 
-num = n**2
+if n%2==0:
+    answer = even_num(n,maps)
+else:
+    answer = odd_num(n, maps)
 
-for j in range(n):
-    if j % 2 == 1:
-        for i in range(n):
-            maps[i][j] = num
-            num -= 1
-    else:
-        for x in range(n-1, -1, -1):
-            maps[x][j] = num
-            num -= 1
-
-for m in maps:
+for m in answer:
     print(*m)
